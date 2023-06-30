bd5fbe8083615f4926cc45f9e1e56f789df1852c
Matt Fairclough <matt.fairclough@gmail.com>
2023-06-30 12:15:26 -0700

    Add comments to exception handling in rpc_examples.py

--- python/examples/rpc_examples.py
+++ python/examples/rpc_examples.py
@@ -17,14 +17,33 @@ try:
         print("Node not found")
 
 except ConnectionError as e:
+    # A built-in Python exception raised by the socket.
+    # This probably means that Terragen isn't running or
+    # there is a misconfiguration in Terragen or this client.
     print("Terragen RPC connection error: " + str(e))
+    # Extra handling here...
+    # ...
 
 except TimeoutError as e:
+    # A built-in Python exception raised either by the socket
+    # or by this module when a socket timeout occurs.
+    # This could mean that the current instance of Terragen failed
+    # to respond or that a previous instance has crashed without
+    # properly closing the socket.
     print("Terragen RPC timeout error: " + str(e))
+    # Extra handling here...
+    # ...
 
 except tg.ReplyError as e:
+    # The server responded, but the data it returned could not be parsed.
     print("Terragen RPC server reply error: " + str(e))
+    # Extra handling here...
+    # ...
 
 except tg.ApiError:
+    # The server responded indicating that there was an API misuse. If
+    # using the High Level API this probably means the server is running
+    # a version of the API that is incompatible with this particular call.
+    # Exception subclasses include ApiMethodNotFound and ApiInvalidParams.
     print("Terragen RPC API error")
     raise
